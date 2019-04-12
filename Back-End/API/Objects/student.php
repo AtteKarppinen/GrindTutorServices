8a592701fcb4aa8d711b814f3d657f7f53d945fd
Atte <t6kaat01@students.oamk.fi>
2019-04-12 18:24:14 +0100

    Registration Functionality For Student
    Pseudo-code for login function as well.

--- Back-End/API/Objects/student.php
+++ Back-End/API/Objects/student.php
@@ -5,8 +5,11 @@
     which initialize an object from this class.
 */
 
+    // Script for validating any user input
+    include_once "../Shared/testInput.php";
+
     class Student {
-    
+
         // Database connection and table name
         private $conn;
         private $tableName = "Student_table";
@@ -17,10 +20,10 @@
         public $s_lname;
         public $s_bdate;
         public $s_sex;
-        public $t_email;
-        public $s_password;     // TODO: Remove, only for developing purposes
-        public $s_address;
-        // public $Location;
+        public $s_email;
+        public $s_password;     
+        //public $s_address;    // Probably not necessary
+        // public $Location;    // Probably not necessary
     
         // Constructor with $db as database connection
         public function __construct($db) {
@@ -40,6 +43,66 @@
             $students->execute();
         
             return $students;
+        }
+
+        // All data is sent in request's body, no need for parameters
+        function register() {
+
+            // Validate data
+            $this->s_fname      = testInput($this->s_fname);
+            $this->s_lname      = testInput($this->s_lname);
+            $this->s_bdate      = testInput($this->s_bdate);
+            $this->s_sex        = testInput($this->s_sex);
+            $this->s_email      = testInput($this->s_email);
+            $this->s_password   = testInput($this->s_password);
+
+            // Check that student is not registered already
+            $user_exists = $this->conn->query("SELECT * FROM $this->tableName WHERE s_email LIKE '$this->s_email'");
+            
+            if ($user_exists->rowCount() === 0) {
+
+                // Insert query 
+                $query = "INSERT INTO Student_table (s_fname, s_lname, s_bdate, s_sex, s_email, s_password)
+                          VALUES(:firstName, :lastName, :birthday, :sex, :email, :hashedPassword)";
+
+                // Prepare insert statement
+                $insert = $this->conn->prepare($query);
+
+                $insert->bindParam(':firstName', $this->s_fname);
+                $insert->bindParam(':lastName', $this->s_lname);
+                $insert->bindParam(':birthday', $this->s_bdate);
+                $insert->bindParam(':sex', $this->s_sex);
+                $insert->bindParam(':email', $this->s_email);
+                // Hash password before storing it
+                $this->s_password = password_hash($this->s_password, PASSWORD_DEFAULT);
+                $insert->bindParam(':hashedPassword', $this->s_password);
+
+                // Send new user to DB
+                try {
+                    $insert->execute();
+                    return true;
+                }
+                catch(PDOException $e) {
+                    return false;
+                }
+            }
+            else {
+                return false;
+            }
+        }
+
+        // Same like in registration, expect data in body
+        function login() {
+
+            // Validate data
+
+            // Fetch data from db with given email
+
+            // Check that row count is greater than 0
+
+            // Check password match
+
+            // Return true/false
         }
     }
 ?>
