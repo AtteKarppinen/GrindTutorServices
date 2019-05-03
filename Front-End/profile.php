7616e1b78d3725dc524c247e00a6a12cb93c60a3
Atte <t6kaat01@students.oamk.fi>
2019-05-03 09:38:23 +0100

    Prevent Access If Not Logged In

--- Front-End/profile.php
+++ Front-End/profile.php
@@ -1,5 +1,10 @@
 <?php
 	include("navigationbar.php");
+
+	// If user has not logged in, redirect to login page
+	if (!(isset($_SESSION["loggedIn"]) && $_SESSION["loggedIn"] === true)) {
+		header("location: login.php");
+	}
 ?>
 
 	<body>
