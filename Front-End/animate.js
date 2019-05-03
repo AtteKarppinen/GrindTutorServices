47bd6a736498af60d8ee389d696dea9f71b31f0d
SarahJane Cregan <sarahc@hotmail.com>
2019-05-03 12:41:00 +0200

    Created the landing page

---
+++ Front-End/animate.js
@@ -0,0 +1,11 @@
+var myVar;
+
+function myFunction() {
+	myVar = setTimeout(showPage, 2000);
+}
+
+function showPage() {
+	window.location.replace("http://localhost/Front-End/index.php")
+	/*http://134.209.83.193/GrindTutorServices/Front-End/index.php*/
+	
+}
