446d43e5ac6178da0683277154829735262a06c6
SeMin <ohsemin4@gmail.com>
2019-05-05 00:04:24 +0100

    login page redirect

--- Front-End/login.php
+++ Front-End/login.php
@@ -1,5 +1,9 @@
 <?php
-	include("navigationbar.php");
+    include("navigationbar.php");
+    if (isset($_SESSION["loggedIn"]) && $_SESSION["loggedIn"] === true) {
+        header('Location: http://134.209.83.193/GrindTutorServices/Front-End/index.php');
+        die();
+    }   
 ?>
 
 	<body>
@@ -33,13 +37,13 @@
                                     <form id="tutorReset" class="modal-content animate">
                                         <h1>Forgot Password?</h1>
                                         <label for="fname">E-mail</label>
-                                        <input type="text" placeholder="Enter your Email" id="Email" required>
+                                        <input type="text" placeholder="Enter your Email" id="tutorResetEmail" required>
                                         
                                         <label for="lname">Date of Birth</label>
                                         <br>
-                                        <input type="date" placeholder="Enter your Date of Birth" id="birthDate" min="1901-01-01" max="2020-12-31" required>
+                                        <input type="date" placeholder="Enter your Date of Birth" id="tutorResetBirthday" min="1901-01-01" max="2020-12-31" required>
                                         <script>
-                                            document.getElementById('birthDate').value = new Date().toISOString().substring(0,10);
+                                            document.getElementById('tutorResetBirthday').value = new Date().toISOString().substring(0,10);
                                         </script>
                                         <button type="submit">Reset</button>
                                     </form>
@@ -61,10 +65,10 @@
 					<div class="content">
                         <form id="studentLog">
                             <label for="email">Email</label>
-                            <input type="email" placeholder="Enter your Email" id="tutorEmail" required>
+                            <input type="email" placeholder="Enter your Email" id="studentEmail" required>
 
                             <label for="password"><b>Password</b></label>
-                            <input type="password" placeholder="Enter Password" id="tutorPassword" required>
+                            <input type="password" placeholder="Enter Password" id="studentPassword" required>
                             <label id="remember">
                                 <input type="checkbox" name="remember"> Remember me
                             </label>
@@ -82,13 +86,13 @@
                                     <form id="studentReset" class="modal-content animate">
                                         <h1>Forgot Password?</h1>
                                         <label for="fname">E-mail</label>
-                                        <input type="text" placeholder="Enter your Email" id="Email" required>
+                                        <input type="text" placeholder="Enter your Email" id="studentResetEmail" required>
                                         
                                         <label for="lname">Date of Birth</label>
                                         <br>
-                                        <input type="date" placeholder="Enter your Date of Birth" id="birthDate" min="1901-01-01" max="2020-12-31" required>
+                                        <input type="date" placeholder="Enter your Date of Birth" id="studentResetBirthday" min="1901-01-01" max="2020-12-31" required>
                                         <script>
-                                            document.getElementById('birthDate').value = new Date().toISOString().substring(0,10);
+                                            document.getElementById('studentResetBirthday').value = new Date().toISOString().substring(0,10);
                                         </script>
                                         <button type="submit">Reset</button>
                                     </form>
