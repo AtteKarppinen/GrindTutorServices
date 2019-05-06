73606c5f7185ca7d7da0d10a7f974d67fa3df30b
AtteKarppinen <33217173+AtteKarppinen@users.noreply.github.com>
2019-05-06 12:37:07 +0100

    Update about.php

--- Front-End/about.php
+++ Front-End/about.php
@@ -11,30 +11,28 @@
 			<div class="aboutText">
 							We know that exam time can be extremely stressfull for students
 							so here at Find a Grind we are bringing some of the best tutors
-							in Ireland to you to make exam time that little bit easier and to help
+							in Dublin to you to make exam time that little bit easier and to help
 							ensure that you get the results that you deserve!
 			</div>
 			
 			<br>
 
-			<img id="aboutImg" src="images/tutor.jpg">
+			<div class="descImg">
+				<img id="roundedImage2" src="Images/Graduating1.jpg" alt="Students">
+			</div>
+			
 			
 			<div class="aboutBody">
 				<p> All the tutors here on Find a Grind can provide grinds for you in every subject
-					at any level which is examinable at secondary level education. 23% of students are currently recieving
-					tutoring which means that 77% of students are not. We believe that all students should have the ability
-					to recieve 1 on 1 tutoring and that this service should be convenient and accessible to every student.
-					<br> 
-				<p> 
-					As this website has been designed by students for students we believe that using
-					this website will help you to overcome the problems that we had encountered when we were
-					also in your position looking for grinds such as quality and location of grinds!
-				</p>
-					Every tutor is also checked to have been Garda Vetted to ensure your security and safety.
+					at any level which is examinable at secondary level education.<br> Every tutor is
+					also checked to have been Garda Vetted to ensure your security and safety.
 				</p>
 				<br>
 				<br>
 				
+				<p> As this website has been designed by students for students we believe that using
+				this website will help you to overcome the problems that we had encountered when we were
+				also in your position looking for grinds such as quality and location of grinds! 
 			</div>
 					
 			
